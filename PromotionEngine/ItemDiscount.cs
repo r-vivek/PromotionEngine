6e0560590dafe1ff9593e1fcf78dec3f0f25392c
Vivek <vivek_r2@dell.com>
2020-06-28 21:24:23 +0530

    Dependent Discounts

---
+++ PromotionEngine/ItemDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class ItemDiscount
+    {
+        public int ItemQuantityInCart { get; set; }
+
+        public int DiscountQuantity { get; set; }
+
+        public int PerItemPrice { get; set; }
+
+        public int RelativeQuantityRule { get; set; }
+
+        public int GrandTotal => (ItemQuantityInCart - (DiscountQuantity * RelativeQuantityRule)) * PerItemPrice;
+    }
+}
--- PromotionEngine/PromotionEngine.cs
+++ PromotionEngine/PromotionEngine.cs
@@ -33,12 +33,50 @@ namespace PromotionEngine
 
             foreach (var rule in rules)
             {
+                // Simple Discounts
                 if (rule.Dependency == null)
                 {
                     var simpleItem = cartItems.First((x => x.Item == rule.Item));
                     var simpleItemTotal = (simpleItem.Quantity / rule.Quantity) * rule.Price +
                                           (simpleItem.Quantity % rule.Quantity) * simpleItem.ItemPrice;
                     runningTotal += simpleItemTotal;
+                }
+                else
+                {
+                    // Dependent Discounts
+                    var discountedQuantityList = new List<int>();
+                    var itemDiscounts = new List<ItemDiscount>();
+                    foreach (var dRule in rule.Dependency)
+                    {
+                        var itemD = cartItems.Where(x => x.Item == dRule.Item).FirstOrDefault();
+                        if (itemD == null)
+                        {
+                            discountedQuantityList.Add(0);
+                            break;
+                        }
+
+                        var discountedQuantity = itemD.Quantity / dRule.Quantity;
+                        discountedQuantityList.Add(discountedQuantity);
+                        itemDiscounts.Add(new ItemDiscount
+                        {
+                            RelativeQuantityRule = dRule.Quantity,
+                            PerItemPrice = itemD.ItemPrice,
+                            DiscountQuantity = 0,
+                            ItemQuantityInCart = itemD.Quantity
+
+                        });
+                    }
+
+                    var commonMultiplier = discountedQuantityList.Min();
+
+                    var discountedTotal = commonMultiplier * rule.Price;
+                    runningTotal += discountedTotal;
+
+                    foreach (var itemDiscount in itemDiscounts)
+                    {
+                        itemDiscount.DiscountQuantity = commonMultiplier;
+                        runningTotal += itemDiscount.GrandTotal;
+                    }
                 }
             }
 
